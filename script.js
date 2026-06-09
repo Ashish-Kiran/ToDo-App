803766a0d9ef635e741cb4fa02112335d5125f39
Ashish Kiran <ashishkiranunacademy@gmail.com>
2026-06-10 01:31:44 +0530

    Styled Version

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 var idx = 1;
-function deleteTodo(idx){
+
+function deleteTodo(idx) {
     const divElement = document.getElementById("todo" + idx);
     document.getElementById("todos").removeChild(divElement);
 }
 
-function todo (){
+function todo() {
     const element = document.getElementById("todoInput");
-    const newDiv1 = document.createElement("div");
-    newDiv1.innerHTML = element.value;
-    if (element.value == ""){
-        return ;
+    if (element.value == "") {
+        return;
     }
-    element.value = "";
-    newDiv1.setAttribute ("id", ("todo" + idx));
+
     const parentDiv = document.getElementById("todos");
+
+    const newDiv1 = document.createElement("div");
+    newDiv1.className = "todo-item";
+    newDiv1.setAttribute("id", "todo" + idx);
+
+    const todoText = document.createElement("div");
+    todoText.className = "todo-text";
+
+    const dot = document.createElement("div");
+    dot.className = "todo-dot";
+
+    todoText.appendChild(dot);
+    todoText.innerHTML += element.value;
+
     const newDiv2 = document.createElement("button");
-    newDiv2.innerHTML = "Delete";
-    newDiv2.setAttribute ("onclick", "deleteTodo(" + idx + ")");
-    newDiv1.appendChild (newDiv2);
-    parentDiv.appendChild (newDiv1);
+    newDiv2.className = "delete-btn";
+    newDiv2.innerHTML = "🗑 Delete";
+    newDiv2.setAttribute("onclick", "deleteTodo(" + idx + ")");
+
+    newDiv1.appendChild(todoText);
+    newDiv1.appendChild(newDiv2);
+    parentDiv.appendChild(newDiv1);
+
+    element.value = "";
     idx++;
 }
 
+document.getElementById("todoInput").addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        todo();
+    }
+});
